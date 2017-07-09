1a2016baa319d63addebff3c50956128f121166f
小发工作室 <451518849@qq.com>
2017-07-09 21:50:51 +0800

    Initial Commit

---
+++ MyFirstPods/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MyFirstPods
+//
+//  Created by 小发工作室 on 2017/7/9.
+//  Copyright © 2017年 小发工作室. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MyFirstPods/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MyFirstPods
+//
+//  Created by 小发工作室 on 2017/7/9.
+//  Copyright © 2017年 小发工作室. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
